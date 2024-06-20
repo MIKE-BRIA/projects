5df9b3271bb7536a5eb23d72ae4e488322e2135c
Brian Michael <brianmichaeladero@gmail.com>
2024-06-20 16:25:30 +0300

    routes

---
+++ portfolioprojects/realestate/Backend/routes/admin.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const adminController = require("../controller/admin.controller");
+
+const router = express.Router();
+
+router.post("/houses", adminController.createNewHouse);
+
+router.get("/houses", adminController.getNewHouse);
+
+router.delete("/houses/:houseId", adminController.deleteNewHouse);
+
+router.get("/houses/:houseId", adminController.getHouseByID);
+
+module.exports = router;
