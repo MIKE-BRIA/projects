3fba25730c86b9b65e5377b6410ca74ee2bf6115
Brian Michael <brianmichaeladero@gmail.com>
2024-06-22 22:14:41 +0300

    store

--- portfolioprojects/realestate/frontend/src/store/store.js
+++ portfolioprojects/realestate/frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import houseReducer from "./slices/houseSlice";
+import agentReducer from "./slices/agentSlice";
 
 const store = configureStore({
   reducer: {
     houses: houseReducer,
+    agents: agentReducer,
   },
 });
 
