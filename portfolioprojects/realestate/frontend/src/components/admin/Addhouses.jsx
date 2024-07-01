b9fb66dbe84d104bb9a713a44be6c4f70972e4d7
Brian Michael <brianmichaeladero@gmail.com>
2024-07-01 22:54:44 +0300

    .\frontend\

--- portfolioprojects/realestate/frontend/src/components/admin/Addhouses.jsx
+++ portfolioprojects/realestate/frontend/src/components/admin/Addhouses.jsx
@@ -18,6 +18,7 @@ export default function Addhouse() {
     yearBuilt: "",
     agent: "",
     agentemail: "",
+    images: [],
     price: "",
     description: "",
   });
@@ -44,6 +45,7 @@ export default function Addhouse() {
           yearBuilt: fetchedHouseData.yearBuilt || "",
           agent: fetchedHouseData.agent || "",
           agentemail: fetchedHouseData.agentemail || "",
+          images: fetchedHouseData.images || [],
           price: fetchedHouseData.price || "",
           description: fetchedHouseData.description || "",
         });
@@ -54,6 +56,8 @@ export default function Addhouse() {
 
     fetchHouseData();
   }, [id]);
+
+  const images = formData.images;
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -88,6 +92,25 @@ export default function Addhouse() {
       ...formData,
       [name]: value,
     });
+  }
+
+  //*uploading images
+  async function uploadImages(e) {
+    const files = e.target?.files;
+
+    if (files?.length > 0) {
+      const data = new FormData();
+
+      for (const file of files) {
+        data.append("file", file);
+      }
+
+      const res = await axios.post("http://localhost:3000/images", data);
+
+      setFormData.images((oldImages) => {
+        return [...oldImages, ...res.data];
+      });
+    }
   }
 
   return (
@@ -264,6 +287,49 @@ export default function Addhouse() {
               className="housedata"
             />
           </div>
+        </div>
+
+        {/* image selection */}
+
+        <div className="mb-2 flex  flex-wrap flex-row gap-1">
+          {Array.isArray(images) && images.length > 0 ? (
+            images.map((link) => (
+              <div className="h-24" key={link}>
+                <img src={link} alt="" className="rounded-lg h-24" />
+              </div>
+            ))
+          ) : (
+            <p className="justify-center">No image selected</p>
+          )}
+
+          <label
+            htmlFor="image"
+            className="w-32 h-32 cursor-pointer border flex flex-col items-center rounded-lg bg-gray-200 justify-center text-gray-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            <div>Upload</div>
+            <input
+              id="image"
+              name="image"
+              type="file"
+              onChange={uploadImages}
+              className="hidden"
+              // accept="png, jpeg"
+            />
+          </label>
         </div>
 
         <label htmlFor="price">Price</label>
