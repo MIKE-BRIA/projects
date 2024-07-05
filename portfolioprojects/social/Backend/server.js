7ad28ad365a9b21410c49928c0a145617be54ec6
Brian Michael <brianmichaeladero@gmail.com>
2024-07-05 15:09:22 +0300

    .\Backend\

---
+++ portfolioprojects/social/Backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongooseConnect from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+mongooseConnect();
+const app = express();
+
+//!middleware
+app.use(express.json()); //to parse json data in the req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data in the req.body
+app.use(cookieParser()); //
+
+//!Routes
+app.use("/api/users", userRoutes);
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`server started at http://localhost:${PORT}`)
+);
