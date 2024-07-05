673c3d8bbd0afc23fbd47f6c41218a137038f1d0
Brian Michael <brianmichaeladero@gmail.com>
2024-07-05 16:35:07 +0300

    server

--- portfolioprojects/social/Backend/server.js
+++ portfolioprojects/social/Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongooseConnect from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 mongooseConnect();
