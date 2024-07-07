ae9440efcbd4f2d17d508042618ed9585152ce41
Brian Michael <brianmichaeladero@gmail.com>
2024-07-07 22:42:38 +0300

    backend

--- portfolioprojects/social/Backend/controllers/user.controller.js
+++ portfolioprojects/social/Backend/controllers/user.controller.js
@@ -86,7 +86,7 @@ export async function followUnfollowUser(req, res) {
     //*the owner of the current account that is on use
     const currentUser = await User.findById(req.user._id);
 
-    if (id === req.user._id)
+    if (id === req.user._id.toString())
       return res
         .status(400)
         .json({ message: "You cannot follow/Unfollow yourself" });
@@ -116,7 +116,32 @@ export async function followUnfollowUser(req, res) {
 
 //!Update User Profile
 export async function updateUser(req, res) {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
   try {
+    let user = await User.findById(userId);
+
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    if (req.params.id !== userId.toString())
+      return res
+        .status(400)
+        .json({ message: "You cannot update other user's Profile" });
+
+    if (password) {
+      const hashedPassword = await brcypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully", user });
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.log("Error in updateUser: ", err.message);
