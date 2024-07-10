e95aa178ed5af34810a6a920b3d65037ae4ee0a4
Brian Michael <brianmichaeladero@gmail.com>
2024-07-10 11:22:44 +0300

    controller

--- portfolioprojects/social/Backend/controllers/user.controller.js
+++ portfolioprojects/social/Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import brcypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
+import { v2 as cloudinary } from "cloudinary";
 
 //!Signup user
 export async function signupUser(req, res) {
@@ -9,7 +10,7 @@ export async function signupUser(req, res) {
     const existingUser = await User.findOne({ $or: [{ email }, { username }] });
 
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ error: "User already exists" });
     }
 
     const hashedPassword = await brcypt.hash(password, 10);
@@ -28,12 +29,14 @@ export async function signupUser(req, res) {
         name: newUser.name,
         email: newUser.email,
         username: newUser.username,
+        bio: newUser.bio,
+        profilePic: newUser.profilePic,
       });
     } else {
-      res.status(400).json({ message: "Invalid user data" });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in signupUser: ", err.message);
   }
 }
@@ -50,7 +53,7 @@ export async function loginUser(req, res) {
     );
 
     if (!user || !isPasswordCorrect)
-      return res.status(400).json({ message: "Invalid username or password" });
+      return res.status(400).json({ error: "Invalid username or password" });
 
     generateTokenAndSetCookie(user._id, res);
 
@@ -59,9 +62,11 @@ export async function loginUser(req, res) {
       name: user.name,
       email: user.email,
       username: user.username,
+      bio: user.bio,
+      profilePic: user.profilePic,
     });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in loginUser: ", err.message);
   }
 }
@@ -72,7 +77,7 @@ export async function logoutUser(req, res) {
     res.cookie("jwt", "", { maxAge: 1 });
     res.status(200).json({ message: "User logged out successfully" });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in Logout User: ", err.message);
   }
 }
@@ -89,10 +94,10 @@ export async function followUnfollowUser(req, res) {
     if (id === req.user._id.toString())
       return res
         .status(400)
-        .json({ message: "You cannot follow/Unfollow yourself" });
+        .json({ error: "You cannot follow/Unfollow yourself" });
 
     if (!userToFollow || !currentUser)
-      return res.status(400).json({ message: "User not found" });
+      return res.status(400).json({ error: "User not found" });
 
     const isFollowing = currentUser.following.includes(id);
 
@@ -109,28 +114,40 @@ export async function followUnfollowUser(req, res) {
       res.status(200).json({ message: "User followed successfully" });
     }
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in followUnfollowUser: ", err.message);
   }
 }
 
 //!Update User Profile
 export async function updateUser(req, res) {
-  const { name, email, username, password, profilePic, bio } = req.body;
+  const { name, email, username, password, bio } = req.body;
+  let { profilePic } = req.body;
   const userId = req.user._id;
   try {
     let user = await User.findById(userId);
 
-    if (!user) return res.status(400).json({ message: "User not found" });
+    if (!user) return res.status(400).json({ error: "User not found" });
 
     if (req.params.id !== userId.toString())
       return res
         .status(400)
-        .json({ message: "You cannot update other user's Profile" });
+        .json({ error: "You cannot update other user's Profile" });
 
     if (password) {
       const hashedPassword = await brcypt.hash(password, 10);
       user.password = hashedPassword;
+    }
+
+    //*uploading profile picture to cloudinary server
+    if (profilePic) {
+      if (user.profilePic) {
+        await cloudinary.uploader.destroy(
+          user.profilePic.split("/").pop().split(".")[0]
+        );
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(profilePic);
+      profilePic = uploadedResponse.secure_url;
     }
 
     user.name = name || user.name;
@@ -143,7 +160,7 @@ export async function updateUser(req, res) {
 
     res.status(200).json({ message: "Profile updated successfully", user });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in updateUser: ", err.message);
   }
 }
@@ -157,11 +174,11 @@ export async function getUserProfile(req, res) {
       .select("-password")
       .select("-updatedAt");
 
-    if (!user) return res.status(400).json({ message: "User not found" });
+    if (!user) return res.status(400).json({ error: "User not found" });
 
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
     console.log("Error in getUserProfile: ", err.message);
   }
 }
