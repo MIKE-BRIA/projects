e33436ce59fa8537983fa633d41951ab24c9d24e
Brian Michael <brianmichaeladero@gmail.com>
2024-07-11 10:53:20 +0300

    frontend

--- portfolioprojects/social/frontend/src/main.jsx
+++ portfolioprojects/social/frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { extendTheme } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import "./index.css";
 
 const styles = {
@@ -33,11 +34,13 @@ const theme = extendTheme({ colors, styles, config });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
