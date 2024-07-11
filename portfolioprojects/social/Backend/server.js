a4527b336ea0c6872d4ca86b522835c500e94e51
Brian Michael <brianmichaeladero@gmail.com>
2024-07-11 06:59:10 +0300

    .\Backend\

--- portfolioprojects/social/Backend/server.js
+++ portfolioprojects/social/Backend/server.js
@@ -4,14 +4,22 @@ import mongooseConnect from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 mongooseConnect();
 const app = express();
 
+//!connecting to cloudinary for image uploads
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 //!middleware
-app.use(express.json()); //to parse json data in the req.body
-app.use(express.urlencoded({ extended: true })); //to parse form data in the req.body
+app.use(express.json({ limit: "50mb" })); //to parse json data in the req.body
+app.use(express.urlencoded({ limit: "50mb", extended: true })); //to parse form data in the req.body
 app.use(cookieParser()); //
 
 //!Routes
