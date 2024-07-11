5ddb136d4f6fd556922415104f6470a87d6e9f52
Brian Michael <brianmichaeladero@gmail.com>
2024-07-11 12:41:27 +0300

    pages

--- portfolioprojects/social/frontend/src/pages/UserPage.jsx
+++ portfolioprojects/social/frontend/src/pages/UserPage.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+
+  const { username } = useParams();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+  }, [username]);
+
   return (
     <>
       <UserHeader />
