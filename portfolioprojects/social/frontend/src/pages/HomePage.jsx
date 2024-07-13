45e944eae3a2b90108d7ff87d4926d6b83108b4d
Brian Michael <brianmichaeladero@gmail.com>
2024-07-13 12:26:27 +0300

    pages

--- portfolioprojects/social/frontend/src/pages/HomePage.jsx
+++ portfolioprojects/social/frontend/src/pages/HomePage.jsx
@@ -1,14 +1,48 @@
-import { Button, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Flex, Spinner } from "@chakra-ui/react";
+// import { Link } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
+import { useEffect, useState } from "react";
+import Post from "../components/Post";
 
 const HomePage = () => {
+  const showToast = useShowToast();
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        console.log(data);
+        if (data.error) return showToast("Error", data.error, "error");
+
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getFeedPosts();
+  }, [showToast]);
   return (
     <>
-      <Link to={"/mike"}>
-        <Flex w={"full"} justifyContent={"center"}>
-          <Button mx={"auto"}>Visit Profile Page</Button>
+      {loading && (
+        <Flex justify={"center"}>
+          <Spinner size={"xl"} />
         </Flex>
-      </Link>
+      )}
+      {!loading && posts.length === 0 && (
+        <Flex justify={"center"}>
+          <h1>Follow some users to get feed</h1>
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
     </>
   );
 };
--- portfolioprojects/social/frontend/src/pages/UserPage.jsx
+++ portfolioprojects/social/frontend/src/pages/UserPage.jsx
@@ -4,11 +4,13 @@ import UserPost from "../components/UserPost";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
   const showToast = useShowToast();
   const { username } = useParams();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUser = async () => {
@@ -21,12 +23,26 @@ const UserPage = () => {
         setUser(data);
       } catch (error) {
         showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
       }
     };
     getUser();
   }, [username, showToast]);
 
-  if (!user) return null;
+  if (!user && loading)
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+
+  if (!user && !loading)
+    return (
+      <Flex justifyContent={"center"}>
+        <h1>User not found</h1>
+      </Flex>
+    );
 
   return (
     <>
