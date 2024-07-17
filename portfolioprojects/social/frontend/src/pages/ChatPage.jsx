570db0ba9c296defa49a0b7e8faf8fb8f996ae64
Brian Michael <brianmichaeladero@gmail.com>
2024-07-17 11:18:55 +0300

    pages

--- portfolioprojects/social/frontend/src/pages/ChatPage.jsx
+++ portfolioprojects/social/frontend/src/pages/ChatPage.jsx
@@ -4,9 +4,14 @@ import {
   Button,
   Flex,
   Input,
+  Skeleton,
+  SkeletonCircle,
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import Conversation from "../components/Conversation";
+import { GiConversation } from "react-icons/gi";
+import MessageContainer from "../components/MessageContainer";
 
 const ChatPage = () => {
   return (
@@ -17,7 +22,6 @@ const ChatPage = () => {
         w={{ base: "100%", md: "80%", lg: "750px" }}
         transform={"translateX(-50%)"}
         p={4}
-        border={"1px solid red"}
       >
         <Flex
           gap={4}
@@ -45,8 +49,48 @@ const ChatPage = () => {
                 </Button>
               </Flex>
             </form>
+
+            {false &&
+              [0, 1, 2, 3, 6, 5].map((_, i) => (
+                <Flex
+                  key={i}
+                  gap={4}
+                  alignItems={"center"}
+                  p={"1"}
+                  borderRadius={"md"}
+                >
+                  <Box>
+                    <SkeletonCircle size={"10"} />
+                  </Box>
+                  <Flex w={"full"} flexDirection={"column"} gap={3}>
+                    <Skeleton h={"10px"} w={"80px"} />
+                    <Skeleton h={"8px"} w={"90%"} />
+                  </Flex>
+                </Flex>
+              ))}
+
+            <Conversation />
+            <Conversation />
+            <Conversation />
           </Flex>
-          <Flex flex={70}>MessageContainer</Flex>
+
+          {false && (
+            <Flex
+              flex={70}
+              borderRadius={"md"}
+              p={2}
+              flexDirection={"column"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              height={"400px"}
+            >
+              <GiConversation size={100} />
+              <Text fontSize={20}>
+                Select a Conversation to start messaging
+              </Text>
+            </Flex>
+          )}
+          <MessageContainer />
         </Flex>
       </Box>
     </>
