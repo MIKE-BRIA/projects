3d3d6f219218d7c8d169423404839b147732143a
Brian Michael <brianmichaeladero@gmail.com>
2024-07-18 05:30:41 +0300

    routes

---
+++ portfolioprojects/social/Backend/routes/message.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import protectRoute from "../middlewares/protectRoute";
+import { sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.post("/", protectRoute, sendMessage);
+
+export default router;
