42f892613439529f4be10f146d06f18105def860
Brian Michael <brianmichaeladero@gmail.com>
2024-07-19 13:04:19 +0300

    controller

--- portfolioprojects/social/Backend/controllers/message.controller.js
+++ portfolioprojects/social/Backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
-import Conversation from "../models/conversationModel";
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/MessageModel.js";
 
+//!Post function for posting messages and conversation to the database
 export async function sendMessage(req, res) {
   try {
     const { recipientId, message } = req.body;
@@ -11,13 +13,84 @@ export async function sendMessage(req, res) {
 
     if (!conversation) {
       conversation = new Conversation({
-        participants: { senderId, recipientId },
+        participants: [senderId, recipientId],
         lastMessage: {
           text: message,
           sender: senderId,
         },
       });
+
+      await conversation.save();
     }
+
+    const newMessage = new Message({
+      conversationId: conversation._id,
+      sender: senderId,
+      text: message,
+    });
+
+    await Promise.all([
+      newMessage.save(),
+      conversation.updateOne({
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      }),
+    ]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+//!getting messages
+export async function getMessages(req, res) {
+  const { otherUserId } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const conversation = await Conversation.findOne({
+      participants: { $all: [userId, otherUserId] },
+    });
+
+    if (!conversation)
+      return res.status(404).json({ error: "No conversation found" });
+
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+//!getting conversation
+export async function getConversations(req, res) {
+  const userId = req.user._id;
+
+  try {
+    const conversations = await Conversation.find({
+      participants: userId,
+    }).populate({
+      path: "participants",
+      select: "username profilePic",
+    });
+
+    if (!conversations)
+      return res.status(404).json({ error: "No conversations found" });
+
+    //*remove current user from the participants array
+    conversations.forEach((conversation) => {
+      conversation.participants = conversation.participants.filter(
+        (participant) => participant._id.toString() !== userId.toString()
+      );
+    });
+
+    res.status(200).json(conversations);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
