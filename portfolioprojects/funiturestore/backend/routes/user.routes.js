3d31c1b4f1401155be0ee693582f7dc86404de88
Brian Michael <brianmichaeladero@gmail.com>
2024-07-27 15:20:31 +0300

    routes

--- portfolioprojects/funiturestore/backend/routes/user.routes.js
+++ portfolioprojects/funiturestore/backend/routes/user.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { signupUser } from "../controllers/user.controller.js";
+import {
+  getUserProfile,
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+router.get("/profile/:id", getUserProfile);
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
