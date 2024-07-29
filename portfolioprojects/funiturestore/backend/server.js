6a99ba87f64b464b1fa79ed4faac8feaa79d0f73
Brian Michael <brianmichaeladero@gmail.com>
2024-07-29 12:38:47 +0300

    backend

--- portfolioprojects/funiturestore/backend/server.js
+++ portfolioprojects/funiturestore/backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongooseConnect from "./db/connectDB.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import UserRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -8,6 +10,14 @@ mongooseConnect();
 
 const app = express();
 const port = 3000;
+
+//!middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+//!Routes
+app.use("/api/users", UserRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
