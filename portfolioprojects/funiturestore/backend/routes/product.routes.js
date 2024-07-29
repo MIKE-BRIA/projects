a49495436852d2c20d43a6cd3862cc51e097e55e
Brian Michael <brianmichaeladero@gmail.com>
2024-07-29 22:14:10 +0300

    routes

--- portfolioprojects/funiturestore/backend/routes/product.routes.js
+++ portfolioprojects/funiturestore/backend/routes/product.routes.js
@@ -1,7 +1,17 @@
 import express from "express";
+import {
+  addProduct,
+  deleteProduct,
+  getProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
+import protectRoute from "../middlewares/protectRoutes.js";
 
 const router = express.Router();
 
-router.post('/add')
+router.get("/getProduct/:id", getProduct);
+router.post("/addProduct", protectRoute, addProduct);
+router.delete("/removeProduct/:id", protectRoute, deleteProduct);
+router.put("/updateProduct/:id", protectRoute, updateProduct);
 
 export default router;
