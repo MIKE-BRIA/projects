3f92a47bf7e38de2d9e4eee7d0ea19e5620382ef
Brian Michael <brianmichaeladero@gmail.com>
2024-07-31 13:18:17 +0300

    frontend

--- portfolioprojects/funiturestore/frontend/src/App.jsx
+++ portfolioprojects/funiturestore/frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import AdminProducts from "./components/AdminProducts";
+import AdminDashboard from "./components/AdminDashboard";
+import AdminOrders from "./components/AdminOrders";
+import AddProduct from "./components/AddProduct";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "signup", element: <Signup /> },
+      { path: "login", element: <Login /> },
+      {
+        path: "admin",
+        element: <Admin />,
+        children: [
+          { path: "dashboard", element: <AdminDashboard /> },
+          { path: "products", element: <AdminProducts /> },
+          { path: "products/add", element: <AddProduct /> },
+          { path: "orders", element: <AdminOrders /> },
+        ],
+      },
+    ],
   },
 ]);
 
--- portfolioprojects/funiturestore/frontend/src/main.jsx
+++ portfolioprojects/funiturestore/frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const styles = {
+  global: (props) => ({
+    body: {
+      color: "black",
+      bg: mode("gray.900", "gray.100")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  },
+};
+
+const theme = extendTheme({ colors, styles, config });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
