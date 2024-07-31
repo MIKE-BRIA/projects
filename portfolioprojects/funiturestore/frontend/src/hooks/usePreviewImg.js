69af2eb9d6164d933d8a84f83fe8f1c4e19ee337
Brian Michael <brianmichaeladero@gmail.com>
2024-07-31 13:17:21 +0300

    hooks

---
+++ portfolioprojects/funiturestore/frontend/src/hooks/usePreviewImg.js
@@ -0,0 +1,37 @@
+//!Hook that is for image previewing when one uploads a new image
+
+import { useState } from "react";
+import useShowToast from "./useShowToast.js";
+
+const usePreviewImg = () => {
+  const [imgUrl, setImgUrl] = useState(null);
+  const showToast = useShowToast();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    console.log("Selected file:", file); // Debugging log
+
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        console.log("FileReader result:", reader.result); // Debugging log
+        setImgUrl(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      showToast(
+        "Invalid file type",
+        "please select an image file type",
+        "error"
+      );
+      setImgUrl(null);
+    }
+  };
+
+  console.log(imgUrl);
+  return { handleImageChange, imgUrl, setImgUrl };
+};
+
+export default usePreviewImg;
