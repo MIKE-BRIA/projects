484661fe7a0f245ff2cdef5ee296d1fd5df0a3c0
Brian Michael <brianmichaeladero@gmail.com>
2024-08-15 21:21:24 +0300

    models

---
+++ portfolioprojects/funiturestore/backend/models/PurchaseModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  productId: { type: "String" },
+  name: { type: "String", required: true },
+  quantity: { type: "String", required: true },
+  price: { type: Number, required: true },
+});
+
+const purchaseSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  products: [productSchema],
+  totalAmount: { type: "Number", required: true },
+  purchaseDate: { type: "Date", required: true, default: Date.now() },
+  paymentMethod: { type: "String" },
+  orderStatus: {
+    type: "String",
+    enum: ["pending", "shipped", "delivered"],
+    default: "pending",
+  },
+  shippingAddress: {
+    street: { type: String },
+    city: { type: String },
+    state: { type: String },
+    zipCode: { type: String },
+    country: { type: String },
+  },
+});
+
+const Purchase = mongoose.model("Purchase", purchaseSchema);
+export default Purchase;
