bc4dbc0c763b4cb40f648274c834b0504da50d36
Brian Michael <brianmichaeladero@gmail.com>
2024-08-19 22:41:45 +0300

    routes

--- portfolioprojects/funiturestore/backend/routes/purchases.routes.js
+++ portfolioprojects/funiturestore/backend/routes/purchases.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   addPurchase,
   getAllPurchase,
+  updatePurchases,
 } from "../controllers/purchases.controller.js";
 
 const router = express.Router();
 
 router.post("/added", addPurchase);
 router.get("/getPurchases", getAllPurchase);
+router.patch("/updateStatus/:purchaseId", updatePurchases);
 
 export default router;
