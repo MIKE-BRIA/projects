c523882c7a732a10b0e77df708461643070c4208
Brian Michael <brianmichaeladero@gmail.com>
2024-08-21 14:30:20 +0300

    pages

--- portfolioprojects/funiturestore/frontend/src/pages/Cart.jsx
+++ portfolioprojects/funiturestore/frontend/src/pages/Cart.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import CartItems from "../components/CartItems";
 import Checkout from "../components/Checkout";
+import { fetchCartItems } from "../store/slices/cartSlice";
+import useUserDetails from "../hooks/useUserDetails";
+import { ClipLoader } from "react-spinners";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const { userDetails, loading, error } = useUserDetails();
+  // const cartItems = useSelector((state) => state.cart.cartItems);
+
+  useEffect(() => {
+    if (userDetails) {
+      const userId = userDetails._id;
+      dispatch(fetchCartItems(userId));
+    }
+  }, [userDetails, dispatch]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <ClipLoader color="#000" loading={true} size={50} />
+      </div>
+    );
+  }
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <>
       <div>
@@ -17,6 +42,7 @@ const Cart = () => {
         </div>
         <div className="flex mt-4 gap-4 p-6 mx-8">
           <div className="w-3/5">
+            {/* Optionally, you can add a check here to ensure cartItems is populated */}
             <CartItems />
           </div>
           <div className="w-2/5">
