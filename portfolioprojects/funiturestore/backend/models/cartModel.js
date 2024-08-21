df3e64c7094d046365e1201368d299f14a14ab28
Brian Michael <brianmichaeladero@gmail.com>
2024-08-21 14:32:19 +0300

    models

---
+++ portfolioprojects/funiturestore/backend/models/cartModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    img: { type: String, default: "" },
+    price: { type: Number, required: true },
+    quantity: { type: Number, default: 0 },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+export default Cart;
